f08739b642426236483a95ee20b52af0e75c330e
prathamesh4512 <prathamesh4512@gmail.com>
2022-08-13 14:28:23 +0530

    create updatePost async thunk

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -23,6 +23,31 @@ export const addNewPost = createAsyncThunk(
   }
 );
 
+export const updatePost = createAsyncThunk(
+  "posts/updatePost",
+  async (initialPost) => {
+    const { id } = initialPost;
+    try {
+      const response = await axios.put(POSTS_URL + `/${id}`, initialPost);
+      return response.data;
+    } catch (err) {
+      //return err.message;
+      return initialPost; // only for testing Redux!
+    }
+  }
+);
+
+// export const deletePost = createAsyncThunk('posts/deletePost', async (initialPost) => {
+//   const { id } = initialPost;
+//   try {
+//       const response = await axios.delete(`${POSTS_URL}/${id}`)
+//       if (response?.status === 200) return initialPost;
+//       return `${response?.status}: ${response?.statusText}`;
+//   } catch (err) {
+//       return err.message;
+//   }
+// })
+
 const postsSlice = createSlice({
   name: "posts",
   initialState,
